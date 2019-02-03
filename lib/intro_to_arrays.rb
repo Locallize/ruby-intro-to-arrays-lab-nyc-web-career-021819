7c56bbc91117ee866332e7860102b097474f80ea
Fillan Lee <fillanlee@aol.com>
2019-02-03 23:59:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,9 +2,21 @@ def instantiate_new_array
   array = []
   array
 end
-array_with_two_elements
-first_element
-third_element
-last_element
-last_element_with_array_methods
-length_of_array
+def array_with_two_elements
+  
+end
+def first_element
+  
+end
+def third_element
+  
+end
+def last_element
+  
+end
+def last_element_with_array_methods
+  
+end
+def length_of_array
+  
+end
