87d056c8352fb671502ddd222b3510f9f6a34d1e
Fillan Lee <fillanlee@aol.com>
2019-02-04 00:01:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,17 +6,17 @@ def array_with_two_elements
   
 end
 def first_element
-  
+  array[0]
 end
 def third_element
-  
+  array[2]
 end
 def last_element
   
 end
 def last_element_with_array_methods
-  
+  array[-1]
 end
 def length_of_array
-  
+  array.length
 end
